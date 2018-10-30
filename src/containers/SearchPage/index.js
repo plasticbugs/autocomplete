019841fe54b6123a06c580f533d74ba3e9e81d0c
Scott Moschella <scottmosch@gmail.com>
2018-10-29 22:13:21 -0700

    Create a container for the SearchPage view to connect to app store

---
+++ src/containers/SearchPage/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {initializeCitiesData, initializeBooksData, bookQuery, cityQuery } from '../App/actions';
+import bookData from '../App/data/books';
+import citiesData from '../App/data/cities';
+
+import SearchPageView from '../../views/SearchPage';
+
+class SearchPage extends Component {
+  render() {
+    return <SearchPageView {...this.props} />
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    citiesData: state.citiesData,
+    booksData: state.booksData,
+    queries: state.queries,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    initializeBooksData: dispatch(initializeBooksData(bookData)),
+    initializeCitiesData: dispatch(initializeCitiesData(citiesData)),
+    storeBookQuery() {
+      dispatch(bookQuery(...arguments))
+    },
+    storeCityQuery() {
+      dispatch(cityQuery(...arguments))
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchPage);
