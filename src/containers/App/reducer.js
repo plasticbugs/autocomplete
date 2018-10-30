dec67e0684f1a983d63e504d5556217fa99613d3
Scott Moschella <scottmosch@gmail.com>
2018-10-30 09:17:18 -0700

    Move actions/reducers/constants into Search container dir

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -1,31 +1,9 @@
 import {combineReducers} from 'redux';
-
 import {
-  INITIALIZE_BOOKS_DATA,
-  INITIALIZE_CITIES_DATA,
-} from './constants';
-
-import {
+  initializeBooksReducer,
+  initializeCitiesReducer,
   storeQuery,
 } from '../SearchPage/reducers'
-
-export function initializeCitiesReducer(state=[], {type, payload}) {
-  switch (type) {
-    case INITIALIZE_CITIES_DATA:
-      return payload;
-    default:
-      return state;
-  }
-};
-
-export function initializeBooksReducer(state=[], {type, payload}) {
-  switch (type) {
-    case INITIALIZE_BOOKS_DATA:
-      return payload;
-    default:
-      return state;
-  }
-};
 
 export const rootReducer = combineReducers({
   citiesData: initializeCitiesReducer,
